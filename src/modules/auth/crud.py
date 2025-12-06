11c535def2f59283d087e079f6a8a679ee66332d
yadavravikantyadav <ravikantyadavamethi1998@gmail.com>
2025-12-06 16:06:16 +0530

    update try

--- src/modules/auth/crud.py
+++ src/modules/auth/crud.py
@@ -5,24 +5,34 @@ from database.models import User
 
 
 def create_user(db: Session, username: str, password: str):
-    user = db.query(User).filter(User.username == username).first()
+    try:
+        user = db.query(User).filter(User.username == username).first()
 
-    if user:
-        raise HTTPException(status_code=400, detail="Username already exists")
+        if user:
+            raise HTTPException(status_code=400, detail="Username already exists")
 
-    hashed_pw = Hash.bcrypt(password)  
+        hashed_pw = Hash.bcrypt(password)  
 
-    new_user = User(username=username, password=hashed_pw)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+        new_user = User(username=username, password=hashed_pw)
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return new_user
+    
+    except Exception as e:
+        print('error',e)
+        return {'status':False,'message':'Something went wrogn'}
 
 
 def authenticate_user(db: Session, username: str, password: str):
-    user = db.query(User).filter(User.username == username).first()
-    if not user:
-        return False
-    if not Hash.verify(password, user.password):  
-        return False
-    return user
+    try:
+        user = db.query(User).filter(User.username == username).first()
+        if not user:
+            return False
+        if not Hash.verify(password, user.password):  
+            return False
+        return user
+    except Exception as e:
+        print('error',e)
+        return {'status':False,'message':'Something went wrogn'}
+
